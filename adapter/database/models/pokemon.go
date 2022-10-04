a1ebe7e971cab3c54dfc982030af4ffa10093b67
Tiago Cambará <tc@learningbank.io>
2022-10-04 18:36:42 +0100

    feat: add slug on pokemon model struct

--- adapter/database/models/pokemon.go
+++ adapter/database/models/pokemon.go
@@ -5,6 +5,7 @@ import "gorm.io/gorm"
 type Pokemon struct {
 	gorm.Model
 	Number     int                `gorm:"not null"`
+	Slug       string             `gorm:"not null"`
 	Name       string             `gorm:"not null"`
 	Status     PokemonStatus      `gorm:"foreignKey:id"`
 	Types      []Type             `gorm:"many2many:pokemon_has_type"`
